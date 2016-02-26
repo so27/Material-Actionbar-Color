6c5751f7f00e7cbfd715dde52ae1f75b56428ffa
Sven Owsianowski <sven@owsianowski.de>
2016-02-26 16:57:29 +0100

    Dateien für B4X
    Die Dateien können mit Basic4Android (www.b4x.com) geöffnet und bearbeitet werden.

---
+++ Material/Objects/src/de/owsianowski/material/designerscripts/LS_page2.java
@@ -0,0 +1,15 @@
+package de.owsianowski.material.designerscripts;
+import anywheresoftware.b4a.objects.TextViewWrapper;
+import anywheresoftware.b4a.objects.ImageViewWrapper;
+import anywheresoftware.b4a.BA;
+
+
+public class LS_page2{
+
+public static void LS_general(java.util.LinkedHashMap<String, anywheresoftware.b4a.keywords.LayoutBuilder.ViewWrapperAndAnchor> views, int width, int height, float scale) {
+anywheresoftware.b4a.keywords.LayoutBuilder.setScaleRate(0.3);
+//BA.debugLineNum = 2;BA.debugLine="AutoScaleAll"[page2/General script]
+anywheresoftware.b4a.keywords.LayoutBuilder.scaleAll(views);
+
+}
+}
